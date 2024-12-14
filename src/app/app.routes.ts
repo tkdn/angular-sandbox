c38c7d978da11225fba0e671a02700fb2604c4df
tkdn <tkdnation@gmail.com>
2024-12-14 22:22:22 +0900

    chore: link to my-form page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'my-form',
+    async loadComponent() {
+      return (await import('./components/my-form/my-form.component'))
+        .MyFormComponent;
+    },
+  },
+];
