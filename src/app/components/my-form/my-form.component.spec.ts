eeb0c64c2f6ba9a0cada59ac29284b7df347b00a
tkdn <tkdnation@gmail.com>
2024-12-15 00:16:10 +0900

    test: my-form testing

--- src/app/components/my-form/my-form.component.spec.ts
+++ src/app/components/my-form/my-form.component.spec.ts
@@ -1,23 +1,65 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { fakeAsync, tick } from '@angular/core/testing';
+import { render, screen } from '@testing-library/angular';
 import { MyFormComponent } from './my-form.component';
+import userEvent from '@testing-library/user-event';
 
 describe('MyFormComponent', () => {
-  let component: MyFormComponent;
-  let fixture: ComponentFixture<MyFormComponent>;
+  it('form is shown', async () => {
+    await render(MyFormComponent);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MyFormComponent]
-    })
-    .compileComponents();
+    const email = screen.getByLabelText('Your email');
+    const pass = screen.getByLabelText('Your password');
 
-    fixture = TestBed.createComponent(MyFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    expect(email).toBeInTheDocument();
+    expect(pass).toBeInTheDocument();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('form valid state', () => {
+    it('first, button is disabled.', fakeAsync(async () => {
+      const { detectChanges } = await render(MyFormComponent);
+      detectChanges();
+      tick();
+
+      const btn = screen.queryByRole('button');
+      expect(btn).toBeDisabled();
+    }));
+
+    it('when inputs are fullfilled, button is able to submit', fakeAsync(async () => {
+      const { detectChanges } = await render(MyFormComponent);
+      const btn = screen.queryByRole('button');
+      const email = screen.getByLabelText<HTMLInputElement>('Your email');
+      const pass = screen.getByLabelText<HTMLInputElement>('Your password');
+
+      await userEvent.type(email, 'foo@bar.exmaple');
+      await userEvent.type(pass, 'deadbeefdeadbeef');
+      detectChanges();
+      tick();
+
+      expect(btn).toBeEnabled();
+    }));
+  });
+
+  describe('error message', () => {
+    it('error is shown, when email is invalid.', fakeAsync(async () => {
+      const { detectChanges } = await render(MyFormComponent);
+      const email = screen.getByLabelText<HTMLInputElement>('Your email');
+
+      await userEvent.type(email, 'foo@');
+      detectChanges();
+      tick();
+
+      expect(screen.queryByText('email is invalid.')).toBeInTheDocument();
+    }));
+
+    it('error is shown, when email is invalid.', fakeAsync(async () => {
+      const { detectChanges } = await render(MyFormComponent);
+      const email = screen.getByLabelText<HTMLInputElement>('Your password');
+
+      await userEvent.type(email, 'deadbeefdeadbee');
+      detectChanges();
+      tick();
+
+      expect(screen.queryByText('password is invalid.')).toBeInTheDocument();
+    }));
   });
 });
