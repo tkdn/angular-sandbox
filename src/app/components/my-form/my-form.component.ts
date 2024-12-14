d73efbbdef577dd77017bb9bbfe687af84e4d658
tkdn <tkdnation@gmail.com>
2024-12-14 23:03:56 +0900

    feat: my form, email and password

--- src/app/components/my-form/my-form.component.ts
+++ src/app/components/my-form/my-form.component.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './my-form.component.html',
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyFormComponent {
+  readonly email = signal('');
+  readonly password = signal('');
 
+  submit() {
+    alert(`email: ${this.email()}, pass: ${this.password()}`);
+  }
 }
