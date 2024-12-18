4aa3cd098bdec23766ae72ce9c624873b46c4128
Satoshi TAKEDA <satoshi.takeda@classi.jp>
2024-12-18 22:11:24 +0900

    chore: fix title, and spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello world!');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello world, angular-sandbox'
+    );
   });
 });
